dbf83a2574d43e07aec1cc28a59a795c23eae750
nicolecattelino <nicolecattelino@gmail.com>
2026-02-23 16:49:33 -0600

    Add files via upload

---
+++ Lab3/lab3.js
@@ -0,0 +1,46 @@
+const map = L.map("map").setView([44.24408087004536, -89.53411348536166], 7);
+
+L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", { // Picked gray basemap because it is uncluttered
+  maxZoom: 20,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+}).addTo(map);
+
+const myIcon = L.icon({
+    iconUrl: 'images/balloon_clipart.png',
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+    popupAnchor: [0, -28],
+});
+
+var monroe = L.marker([42.6022742,-89.62834953], {icon: myIcon})
+  .bindPopup("<b>Event name: Monroe Balloons and Bands Festival</b><br>Time of event: Mid-June").addTo(map);
+
+var rns = L.marker([43.56667519,-90.88702085], {icon: myIcon})
+  .bindPopup("<b>Event name: Rise & Shine Balloon Rally</b><br>Time of event: Late June").addTo(map);
+
+var tng = L.marker([44.97397585,-89.796432], {icon: myIcon})
+  .bindPopup("<b>Event name: Taste N' Glow Balloon Fest</b><br>Time of event: Mid-July").addTo(map);
+
+var dairy = L.marker([44.70859672,-90.1258743], {icon: myIcon})
+  .bindPopup("<b>Event name: Dairyland Balloon Jubilee</b><br>Time of event: Mid-July").addTo(map);
+
+var paper = L.marker([44.27282424,-88.34700422], {icon: myIcon})
+  .bindPopup("<b>Event name: Paperfest</b><br>Time of event: Mid-July").addTo(map);
+
+var eaa = L.marker([43.9798155,-88.56752573], {icon: myIcon})
+  .bindPopup("<b>Event name: EAA Airventure</b><br>Time of event: Late July").addTo(map);
+
+var salute = L.marker([45.67854072,-89.42352885], {icon: myIcon})
+  .bindPopup("<b>Event name: Saluting Heroes Balloon Rally</b><br>Time of event: Early August").addTo(map);
+
+var lake = L.marker([44.0916171,-87.65644432], {icon: myIcon})
+  .bindPopup("<b>Event name: Lakeshore Balloon Rally</b><br>Time of event: Early August").addTo(map);
+
+var burger = L.marker([44.50726762,-88.33742266], {icon: myIcon})
+  .bindPopup("<b>Burgerfest</b><br>Time of event: Mid-August").addTo(map);
+
+var summer = L.marker([44.22947193,-88.3316257], {icon: myIcon})
+  .bindPopup("<b>Event name: Summer In The Park - Flight Night</b><br>Time of event: Mid-August").addTo(map);
+
+var igNight = L.marker([44.51909157,-88.01641863], {icon: myIcon})
+  .bindPopup("<b>Event name: igNight Market</b><br>Time of event: Mid-August").addTo(map);
